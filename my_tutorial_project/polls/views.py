b57b19cce2dae1662c58f14e1343b356debaeed0
elihayc <elihayc@gmail.com>
2022-08-19 21:15:14 +0300

    tutorial part3

--- my_tutorial_project/polls/views.py
+++ my_tutorial_project/polls/views.py
@@ -1,5 +1,55 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world, You're at poll index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    # using shortcut instead of creating template object
+    return render(request, 'polls/index.html', context)
+
+
+def indexAsString(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def indexNoShortCutRender(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    #404 shortcut
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
